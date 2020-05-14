b46af5423238f53433d803b29db410b1d868d067
Farhana Haq <haqshoma12@gmail.com>
2020-05-13 22:27:19 -0500

    2nd commit ~ added my POMProject

--- src/main/java/util/BrowserFactory.java
+++ src/main/java/util/BrowserFactory.java
@@ -6,19 +6,22 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BrowserFactory {
-	
+
+	//declaring class level webdriver
 	static WebDriver driver;
 	
+	//creating a non-void method to call chrome browser and go to destination site and return a WebDriver
 	public static WebDriver launchBrowser() {
-		
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\14693\\eclipse-workspace-Selenium\\POMProject_Farhana\\Drivers\\chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\14693\\eclipse-workspace-Selenium\\POMProject_Farhana\\Drivers\\chromedriver.exe"); //locate chrome .exe files
+		driver = new ChromeDriver(); //driver is a chrome driver
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);//wait for 10 sec before timeout
 		driver.get("http://techfios.com/test/billing/?ng=admin/");
-		
+		driver.manage().window().maximize();
 		return driver;
 		
+		
 	}
-
+	
+	
+	
 }
