d5e1a58b0a8559006045e28893ce9410387ac23c
mandiosho <60794614+mandiosho@users.noreply.github.com>
2020-05-19 10:36:27 -0500

    commit2

--- src/main/java/page/DashBoardPage.java
+++ src/main/java/page/DashBoardPage.java
@@ -6,17 +6,25 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
 public class DashBoardPage {
-	
-WebDriver driver;
-	
+
+	WebDriver driver;
+
 	public DashBoardPage(WebDriver driver) {
 		this.driver = driver;
 	}
-	
-	
-	//Element Library
-	/*WebElement WEBELEMENT_USERNAME_FIELD_LOCATOR = driver.findElement(By.id("username"));
-	By BY_USERNAME_FIELD_LOCATOR = By.id("username");*/
-	@FindBy(how = How.XPATH, using = "//span[contains(text(), 'Dashboard')]") WebElement DASHBOARD_LOCATOR;
+
+	// Element Library
+	/*
+	 * WebElement WEBELEMENT_USERNAME_FIELD_LOCATOR =
+	 * driver.findElement(By.id("username")); By BY_USERNAME_FIELD_LOCATOR =
+	 * By.id("username");
+	 */
+	@FindBy(how = How.XPATH, using = "//span[contains(text(), 'Dashboard')]")
+	WebElement DASHBOARD_LOCATOR;
+	// method to interact with element
+	public void verifyDashboardTab() {
+		wetForElement(driver,2,DASHBOARD_LOCATOR);
+		
+	}
 
 }
