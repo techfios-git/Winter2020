05bd7690ad394e2b78706177257fdc2edb324644
Levi Campbell <sinbad1422@gmail.com>
2020-06-29 17:41:31 -0500

    Add files via upload
    Exam question 3

---
+++ Examfinal/Test3/src/main/java/Techfios/com/Test3/App.java
@@ -0,0 +1,13 @@
+package Techfios.com.Test3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
