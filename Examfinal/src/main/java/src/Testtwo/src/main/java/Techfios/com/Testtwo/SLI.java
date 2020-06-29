fca1b9b8b52228eb98bbab8f5f9b67fe75b6e0b9
Levi Campbell <sinbad1422@gmail.com>
2020-06-29 17:17:32 -0500

    Add files via upload
    Exam 2nd question

---
+++ Examfinal/src/main/java/src/Testtwo/src/main/java/Techfios/com/Testtwo/SLI.java
@@ -0,0 +1,60 @@
+package Techfios.com.Testtwo;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SLI {
+	
+				
+				static String browser = "Firefox";
+				static WebDriver driver = null;
+
+				public static void main(String[] args) throws Exception {
+
+					if (browser.contentEquals("Chrome")) {
+						System.setProperty("webdriver.chrome.driver",
+								"\\C:\\Users\\BlackHole\\Desktop\\Project_Automation\\Examfinal\\Drivers\\chromedriver.exe");
+						driver = new ChromeDriver();
+					}
+
+					else if (browser.equalsIgnoreCase("Firefox"))
+
+					{
+						System.setProperty("webdriver.gecko.driver",
+								"\\C:\\Users\\BlackHole\\Desktop\\Project_Automation\\Examfinal\\Drivers\\geckodriver.exe");
+						driver = new FirefoxDriver();
+					}
+					// control window size
+					driver.manage().window().maximize();
+
+					// driver.manage()deleteAllCookies();
+					driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+					// go to the link
+					driver.navigate().to("http://techfios.com/test/101/");
+					 Thread.sleep(6000);
+					driver.manage().timeouts().implicitlyWait(90, TimeUnit.SECONDS);
+					
+					driver.findElement(By.name("todo[2]")).click();
+					Thread.sleep(9000);
+					driver.findElement(By.name("submit")).click();
+					  //driver.findElement(By.name("allbox")); WebElement ToggleAll =
+					  //driver.findElement(By.name("allbox")); Boolean checkToggleAll =
+					  //ToggleAll.isSelected();
+					  
+					  //if (checkToggleAll == false) {
+					  //System.out.println(" Toggle All is not checked!"); } else {
+					  //System.out.println("Toggle All is checked!");
+					  
+					  }
+					 
+
+		
+
+	}
+
+
