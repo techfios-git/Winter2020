528c01e6b6fbe706c480d6f9834084ad1dbf350f
Levi Campbell <sinbad1422@gmail.com>
2020-06-29 19:42:43 -0500

    Add files via upload

---
+++ Examfinal/addcategory/src/main/java/Techfios/com/addcategory/addlevi1.java
@@ -0,0 +1,63 @@
+package Techfios.com.addcategory;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+import java.util.concurrent.TimeUnit;
+
+public class addlevi1 {
+	
+	static String browser = "Firefox";
+	static WebDriver driver = null;
+
+	public static void main(String[] args) throws Exception {
+
+						if (browser.contentEquals("Chrome")) {
+							System.setProperty("webdriver.chrome.driver",
+									"\\C:\\Users\\BlackHole\\Desktop\\Project_Automation\\Examfinal\\Drivers\\chromedriver.exe");
+							driver = new ChromeDriver();
+						}
+
+						else if (browser.equalsIgnoreCase("Firefox"))
+
+						{
+							System.setProperty("webdriver.gecko.driver",
+									"\\C:\\Users\\BlackHole\\Desktop\\Project_Automation\\Examfinal\\Drivers\\geckodriver.exe");
+							driver = new FirefoxDriver();
+						}
+						// control window size
+						driver.manage().window().maximize();
+
+						// driver.manage()deleteAllCookies();
+						driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+						// go to the link
+						driver.navigate().to("http://techfios.com/test/101/");
+						 Thread.sleep(6000);
+						driver.manage().timeouts().implicitlyWait(90, TimeUnit.SECONDS);
+						
+						//driver.findElement(By.name("todo[2]")).click();
+						Thread.sleep(9000);
+						//driver.findElement(By.name("submit")).click();
+						//driver.findElement(By.name("allbox")); WebElement ToggleAll =
+						  driver.findElement(By.name("categorydata")).sendKeys("Levi's Data");
+						  Thread.sleep(9000);
+						  //driver.findElement(By.name("submit")).click();
+						  //driver.findElement(By.xpath([@id="extra"]/input[2]).click();
+						  driver.findElement(By.xpath(("//input[@value='Add category']"))).click();
+						  //driver.findElement(By.name("allbox")); Boolean checkToggleAll =
+						  //ToggleAll.isSelected();
+						  
+						  //if (checkToggleAll == false) {
+						  //System.out.println(" Toggle All is not checked!"); } else {
+						  //System.out.println("Toggle All is checked!");
+						  
+						  }
+
+}
+
+
+
